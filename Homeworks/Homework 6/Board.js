5d1bfcb2720e465075b240377642e244e9eeb40a
JEHatred <jmota@ucol.mx>
2019-09-14 22:07:09 -0500

    Full game button added // Play turn button added // Winner system & reset position system added

--- Homeworks/Homework 6/Board.js
+++ Homeworks/Homework 6/Board.js
@@ -2,4 +2,8 @@ export default class Board{
     constructor(large){
         this._large = large;
     }
+
+    get large(){
+        return this._large;
+    }
 }
--- Homeworks/Homework 6/Game.js
+++ Homeworks/Homework 6/Game.js
@@ -2,60 +2,106 @@ import Board from "./Board.js";
 import Player from "./Player.js";
 import Buff from "./Buff.js";
 
-class Game{
-    constructor(){
-        this._turn = true;//Juega player 1
-        this._board = new Board();
-        this._player1 = new Player("EN");
-        this._player2 = new Player("JE");
-        this._stairs = [
-            this._stair1 = new Buff(1, 10),
-            this._stair2 = new Buff(2, 10),
-            this._stair3 = new Buff(3, 10),
-            this._stair4 = new Buff(4, 10),
-            this._stair5 = new Buff(5, 10),
-            this._stair6 = new Buff(6, 10)
-        ];
-        this._snakes = [
-            this._snake1 = new Buff(10, -10),
-            this._snake2 = new Buff(20, -10),
-            this._snake3 = new Buff(30, -10),
-            this._snake4 = new Buff(40, -10),
-            this._snake5 = new Buff(50, -10),
-            this._snake6 = new Buff(60, -10)
-        ];
+document.querySelector("#btnTurn").addEventListener("click", playTurn);
+document.querySelector("#btnFullGame").addEventListener("click", playFullGame);
 
-        document.querySelector("#btnTest").addEventListener("click", () => {
-            this.playTurn();
-        });
-    }
+let turn = true;//Juega player 1
+let numTurn = 0;
+let board = new Board(50);
+let player1 = new Player("EN");
+let player2 = new Player("JE");        
+let buffs = [
+    new Buff(1, 10),
+    new Buff(2, 10),
+    new Buff(3, 10),
+    new Buff(4, 10),
+    new Buff(5, 10),
+    new Buff(6, 10),
+    new Buff(10, -10),
+    new Buff(20, -10),
+    new Buff(30, -10),
+    new Buff(40, -10),
+    new Buff(50, -10),
+    new Buff(60, -10)
+];
 
-    playTurn(){
-        if(this._turn == true){
-            this._player1.moveAlong();
-            console.log("Se jugo el turno de EN. Posicion = " + this._player1.position);
-            this._turn = false;
-            return this._player1.position;
+function playFullGame(){
+    while(player1.position<board.large && player2.position<board.large){
+        if(turn == true){
+            player1.moveAlong();
+            console.log(numTurn + " Se jugo el turno de " + player1.name + ". Posicion actual = " + player1.position);
+            turn = false;
+            numTurn++;
         }else{
-            this._player2.moveAlong();
-            console.log("Se jugo el turno de JE. Posicion = " + this._player2.position);
-            this._turn = true;
-            return this._player2.position;;
+            player2.moveAlong();
+            console.log(numTurn + " Se jugo el turno de " + player2.name + ". Posicion actual = " + player2.position);
+            turn = true;
+            numTurn++;
         }
     }
-
-    /*checkBuff(){
-        if(this._turn == true){
-            if(this._player1.position == this._stairs){
-                this._player1.position+=this._stairs.position;
-                console.log("Cayo escalera, nueva posicion = " + this._player1.position);
-            }
-            //return this._player1.position;
-        }else{
-            //return this._player2.position;
-        }
-    }*/
+    console.log(gameOver());
 }
 
-let game = new Game();
-game;
+function playTurn(){
+    if(turn == true){
+        player1.moveAlong();
+        console.log(player1.name + " jugo el turno " + numTurn + ". Posicion actual = " + player1.position);
+        turn = false;
+        numTurn++;
+    }else{
+        player2.moveAlong();
+        console.log(player2.name + " jugo el turno " + numTurn + ". Posicion actual = " + player2.position);
+        turn = true;
+        numTurn++;
+    }
+    
+    if(player1.position>=board.large || player2.position>=board.large){
+        console.log(gameOver());
+    }
+}
+
+function gameOver(){
+    let w =  winner();
+    restartPosition();
+    return w;
+}
+
+function restartPosition(){
+    player1.position = 0;
+    player2.position = 0;
+    numTurn = 0;
+    return;
+}
+
+function winner(){
+    if(player1.position >= board.large){   
+        let p1Winner = "Ganador: " + player1.name;
+        return p1Winner;
+    }else{
+        let p2Winner = "Ganador: " + player2.name;
+        return p2Winner;
+    }
+}
+
+/*checkBuff(){
+    if(turn == true){
+        if(player1.position == board.findBuff(player1.position)){
+            console.log("Buff, nueva posicion = " + player1.position);
+            return player1.position;
+        }
+    }else{
+        if(player2.position == board.findBuff(player2.position)){
+            player2.position += board.findBuff(player2.position);
+            console.log("Buff, nueva posicion = " + player2.position);
+            return player2.position;
+        }
+    }
+}*/
+
+/*findBuff(location){
+    for(let i = 0; i<=buffs.length; i++){
+        if(location == buffs[i]._bPosition){
+            return i;
+        }
+    }
+}*/
